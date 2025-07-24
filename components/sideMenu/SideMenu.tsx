c4f972f0b1741aab5f9504e59c0ff31954ee5b7d
EhsanRe <ers6510@gmail.com>
2025-07-24 13:00:47 -0700

    side menu and home page

---
+++ components/sideMenu/SideMenu.tsx
@@ -0,0 +1,14 @@
+'user client';
+
+import "./side-menu.css"
+
+export default function SideMenu() {
+  return (
+    <aside className="side-style">
+      <h2 className="text-xl font-bold mb-4">Dashboard</h2>
+      <ul>
+        <li>Users</li>
+      </ul>
+    </aside>
+  )
+};
