840f2a85802cde1731f9f6ebfccec6638300eff0
EhsanRe <ers6510@gmail.com>
2025-07-25 11:26:39 -0700

    Styling Sidebar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ children }: { children: React.ReactNode }) {
   return (
-    <header className="flex h-12 items-center mb-3">
+    <header className="flex h-12 items-center mb-3 justify-center md:justify-start">
         {children}
     </header>
   )
--- components/sideMenu/SideMenu.tsx
+++ components/sideMenu/SideMenu.tsx
@@ -1,23 +1,38 @@
-'user client';
+'use client';
 
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import "./side-menu.css";
 
 export default function SideMenu() {
-  return (
-    <aside className="side-style">
-      <h2 className="text-xl font-bold mb-4">Dashboard</h2>
-      <ul>
-        <li className="item">
-          <Image src="/images/users.png" alt="Description of my image" width={25} height={20}/>
-          <Link className="w-full" href="/users">Users</Link>
-        </li>
-        <li className="item">
-          <Image src="/images/add-user.png" alt="Description of my image" width={25} height={20}/>
-          <Link className="w-full" href="/users">Create User</Link>
-        </li>
-      </ul>
-    </aside>
-  )
+    const [isOpen, setIsOpen] = useState<boolean>(true);
+
+    const onClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="relative">
+            <div onClick={onClick} className={isOpen ? "side-button hidden" : "side-button"}>
+                <div className="font-extrabold text-2xl">{isOpen ? '<' : '>'}</div>
+            </div>
+            <aside className={isOpen ? "side-style qqq" : "side-style"}>
+                <div className="flex flex-row justify-between">
+                    <h2 className="text-xl font-bold mb-4">Dashboard</h2>
+                    <div onClick={onClick} className="close-button">x</div>
+                </div>
+                <ul>
+                    <li className="item">
+                        <Image src="/images/users.png" alt="Description of my image" width={25} height={20}/>
+                        <Link onClick={onClick} className="w-full" href="/users">Users</Link>
+                    </li>
+                    <li className="item">
+                        <Image src="/images/add-user.png" alt="Description of my image" width={25} height={20}/>
+                        <Link className="w-full" href="/users">Create User</Link>
+                    </li>
+                </ul>
+            </aside>
+        </div>
+    )
 };
