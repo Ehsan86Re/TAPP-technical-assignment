cbb5bbc6e6c67440f8522728e82e6bcc2ce4136f
EhsanRe <ers6510@gmail.com>
2025-07-25 12:48:06 -0700

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/header/Header";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold">Home Page</h1>
+      <Header>
+        <h1 className="font-bold">Home Page</h1>
+      </Header>
+      <p>Hello 👋</p>
+      <br />
+      <p>Welcome to my simple dashboard displaying some sample user data.</p>
+      <p>To see the user list, just click on 'Users' in the sidebar ...</p>
     </div>
   );
 }
